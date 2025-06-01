b200dd1818cef2f852455b53e4a996e548508652
sunilsala88 <sunilsala88@gmail.com>
2025-06-01 18:18:50 +0530

    jun1-5

--- 5_loops/while1.py
+++ 5_loops/while1.py
@@ -7,3 +7,22 @@ while True:
     i=i+1
 
 #get me all even numbers between 1 and 100
+
+l1=[]
+i=0
+while True:
+    if i>100:
+        break
+    i=i+1
+    if i%2==0:
+        l1.append(i)
+print(l1)
+
+#multiplication table
+num1=3
+num2=1
+while True:
+    if num2==11:
+        break
+    print(num1,'X',num2,num1*num2)
+    num2=num2+1
