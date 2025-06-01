9709ea8eaa4c1160a12d3dc4689cfa06e2abdfee
sunilsala88 <sunilsala88@gmail.com>
2025-06-01 17:02:00 +0530

    jun1-2

--- 5_loops/loop1.py
+++ 5_loops/loop1.py
@@ -15,6 +15,23 @@ print(wallet/num)
 
 l2=(4,5,6)
 t=0
-for i in l2:
-    t=t+i
+for m1 in l2:
+    t=t+m1
 print(t/len(l2))
+
+#type 2 loop
+print('hello')
+print(list(range(100)))
+
+for i in range(10):
+    print('hello',i)
+#generate a list of 50 even numbers
+
+l1=[]
+for i in range(100):
+    if i%2==0:
+        l1.append(i)
+print(l1)
+
+for i in range(1):
+    print(i)
