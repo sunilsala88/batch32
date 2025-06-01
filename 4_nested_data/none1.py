4988ee5e1e60a5017389e2d7fa46c6e1f4975c91
sunilsala88 <sunilsala88@gmail.com>
2025-06-01 16:50:44 +0530

    jun1-1

---
+++ 4_nested_data/none1.py
@@ -0,0 +1,24 @@
+
+
+
+d1={'tcs':600,'amzn':780,'goog':890}
+print(d1)
+print(d1.get('tcs',99))
+# print(d1['tcs1'])
+
+a=None
+print(a)
+print(type(a))
+
+a=12
+b=11
+
+#truthy value
+
+#falsy value
+#"",0,[],{},(),None
+
+if None:
+    print('inside if')
+else:
+    print('inside else')
---
+++ 5_loops/loop1.py
@@ -0,0 +1,20 @@
+
+
+#looping ,iteration
+#iterable
+l1=[10,10,10,10,10]
+
+#type 1 loop
+wallet=0
+for i in l1:
+    wallet=wallet+i
+print(wallet)
+num=len(l1)
+print(num)
+print(wallet/num)
+
+l2=(4,5,6)
+t=0
+for i in l2:
+    t=t+i
+print(t/len(l2))
