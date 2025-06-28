01ce9c0fb91b3b4ec0aaea1ec05e5ab4d4a24e93
sunilsala88 <sunilsala88@gmail.com>
2025-06-28 17:40:41 +0530

    jun-28-3

--- 10_module/modeule1.py
+++ 10_module/modeule1.py
@@ -11,10 +11,23 @@ import sys
 # print(random.randint(10,20))
 # print(os.getcwd())
 
-while True:
-    a=input('enter something')
-    if a=='q':
-        print('exit')
-        sys.exit()
+# while True:
+#     a=input('enter something')
+#     if a=='q':
+#         print('exit')
+#         sys.exit()
 
-print('last line')
+# print('last line')
+
+import selenium
+
+#install
+#pip install module_name
+#uninstall
+#pip uninstall module_name
+#list of modules
+#pip list/freeze
+#downgrading a module
+#pip install module_name==version
+#ugrade a module
+#pip install --upgrade module_name
