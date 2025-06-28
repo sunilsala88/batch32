915c6c1a7af1f995fe3f7806c1bd942ff98a0a42
sunilsala88 <sunilsala88@gmail.com>
2025-06-28 16:50:26 +0530

    jun-28-1

---
+++ 10_module/alkdjflkajsdlkfjasdfkjlakdfj.py
@@ -0,0 +1,8 @@
+
+a=100
+
+def demo():
+    return 'abc'
+
+class Sample:
+    t1=500
---
+++ 10_module/file2.py
@@ -0,0 +1,19 @@
+# import file1
+
+# print(file1.a)
+# print(file1.demo())
+
+# b1=file1.Sample()
+# print(b1.t1)
+
+# import alkdjflkajsdlkfjasdfkjlakdfj as f1
+# print(f1.a)
+# print(f1.demo())
+
+# b1=f1.Sample()
+# print(b1.t1)
+
+from alkdjflkajsdlkfjasdfkjlakdfj import a,demo
+
+print(a)
+print(demo())
