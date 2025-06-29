f13c6676ddbbff55b9cc69299cb3499944c943a5
sunilsala88 <sunilsala88@gmail.com>
2025-06-29 16:55:01 +0530

    jun-29-1

---
+++ 10_module/dete1.py
@@ -0,0 +1,16 @@
+import time
+import random
+
+import datetime
+
+print(random.randint(100,200))
+
+d1='2025-01-01'
+
+
+dt1=datetime.datetime(2025,6,29)
+print(dt1.weekday())
+
+
+
+print(dt1+datetime.timedelta(days=1))
