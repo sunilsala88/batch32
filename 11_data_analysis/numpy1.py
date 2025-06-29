18e003874f36e9a345bf5589225583a0dd6b1b44
sunilsala88 <sunilsala88@gmail.com>
2025-06-29 17:40:47 +0530

    jun-29-4

--- 11_data_analysis/numpy1.py
+++ 11_data_analysis/numpy1.py
@@ -29,6 +29,16 @@
 
 # print(npm[0:2,0:2])
 import numpy as np
-a=np.arange(25).reshape(5,5)
-print(a)
-print(a[1:3,2:4])
+# a=np.arange(25).reshape(5,5)
+# print(a)
+# print(a[1:3,2:4])
+# print(a[2:4,1:4])
+# b=np.random.randint(1,100,36).reshape(6,6)
+# print(b)
+
+
+m1=np.arange(16).reshape(4,4)
+print(m1)
+import pandas as pd
+df1=pd.DataFrame(m1,index=['r1','r2','r3','r4'],columns=['c1','c2','c3','c4'])
+print(df1)
