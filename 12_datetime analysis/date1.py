81cc3e42e65051ce24d8d8950c99c4d273f11562
sunilsala88 <sunilsala88@gmail.com>
2025-07-13 17:37:14 +0530

    jul13-3

---
+++ 12_datetime analysis/date1.py
@@ -0,0 +1,22 @@
+
+
+data='2024-01-01'
+
+import datetime as dt
+
+#date
+#time
+#datetime
+
+d1=dt.date(2025,1,1)
+print(d1)
+
+t1=dt.time(11,15,15)
+print(t1)
+
+dt1=dt.datetime(2025,7,12,11,15,15)
+print(dt1.weekday())
+#0 mon
+#1 tue
+#2 wed
+#6 sun
