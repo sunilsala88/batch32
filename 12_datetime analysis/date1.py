93c65983e15ec178106aee89cc2e00b4fc7eeb01
sunilsala88 <sunilsala88@gmail.com>
2025-07-14 11:25:45 +0530

    jul13-4

--- 12_datetime analysis/date1.py
+++ 12_datetime analysis/date1.py
@@ -14,9 +14,37 @@ print(d1)
 t1=dt.time(11,15,15)
 print(t1)
 
-dt1=dt.datetime(2025,7,12,11,15,15)
-print(dt1.weekday())
+dt1=dt.datetime(2025,7,13,11,15,15)
+print(dt1)
 #0 mon
 #1 tue
 #2 wed
 #6 sun
+
+i1=dt.timedelta(minutes=2)
+print(dt1+i1)
+
+
+#convert datetime to epoch
+print(dt1.timestamp())
+
+#epoch to datetime
+n=1752409073
+a=dt.datetime.fromtimestamp(n)
+print(a)
+
+#datetime to string
+
+# Sunday, 13 July 2025 12:17:53
+
+f='%A, %d %B %Y %H:%M:%S'
+s1=dt1.strftime(f)
+print(s1)
+#string to datetime
+
+
+s2="Sunday, 13 July 2025 12:17:53"
+f='%A, %d %B %Y %H:%M:%S'
+
+d5=dt.datetime.strptime(s2,f)
+print(d5)
