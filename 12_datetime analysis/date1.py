149d8ecf3ca986d2c8fd79636b5a507e85000117
sunilsala88 <sunilsala88@gmail.com>
2025-07-19 16:56:36 +0530

    jul-19-1

--- 12_datetime analysis/date1.py
+++ 12_datetime analysis/date1.py
@@ -48,3 +48,32 @@ f='%A, %d %B %Y %H:%M:%S'
 
 d5=dt.datetime.strptime(s2,f)
 print(d5)
+
+
+
+
+#all thur of year 2025
+
+# start=dt.datetime(2025,1,1)
+# for i in range(365):
+    
+#     if start.weekday()==3:
+#         print(start)
+#     start=start+dt.timedelta(days=1)
+
+#creart a fun which shuld giver me current expiry
+def get_nearest_expiry(d1):
+    start=dt.datetime(2025,1,1)
+    for i in range(365):
+    
+        if start.weekday()==3:
+            print(start,(start-d1).days)
+            if (start-d1).days>0:
+                return start
+        start=start+dt.timedelta(days=1)
+
+# d1=dt.datetime.now()
+d1=dt.datetime(2025,12,24)
+print(d1)
+ans=get_nearest_expiry(d1)
+print(ans)
