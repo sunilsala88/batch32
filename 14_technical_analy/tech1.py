15edf8a4eb074b802c119b5365679511c29f5cb5
sunilsala88 <sunilsala88@gmail.com>
2025-07-27 17:30:55 +0530

    jul-27-4

--- 14_technical_analy/tech1.py
+++ 14_technical_analy/tech1.py
@@ -17,5 +17,15 @@ data
 # print(data)
 
 
-data['ema']=ta.ema(data['Close'],10)
-print(data)
+# data['ema']=ta.ema(data['Close'],10)
+# print(data)
+
+d=ta.bbands(data['Close'],20)
+print(d)
+
+import mplfinance as mpf
+a=mpf.make_addplot(d['BBL_20_2.0'],color='black')
+b=mpf.make_addplot(d['BBM_20_2.0'],color='blue')
+c=mpf.make_addplot(d['BBU_20_2.0'],color='green')
+mpf.plot(data,type='candle',style='yahoo',addplot=[a,b,c])
+
