b62fb8e8f44a2bb48c8fef6634db0e53abca3152
sunilsala88 <sunilsala88@gmail.com>
2025-07-27 17:16:36 +0530

    jul-27-2

--- 14_technical_analy/tech1.py
+++ 14_technical_analy/tech1.py
@@ -5,3 +5,17 @@ print(data)
 
 # import talib
 # import pandas_ta
+
+
+import pandas_ta as ta
+data['sma']=ta.sma(data['Close'],length=10)
+data
+
+
+# import talib as ta
+# data['sma2']=ta.SMA(data['Close'],10)
+# print(data)
+
+
+data['ema']=ta.ema(data['Close'],10)
+print(data)
