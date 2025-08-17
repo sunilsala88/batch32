632f01453b91cb03e17693083ce6bae6c22b7c34
sunilsala88 <sunilsala88@gmail.com>
2025-08-17 17:04:19 +0530

    aug-17-2

--- 15_broker_api/template_stock.py
+++ 15_broker_api/template_stock.py
@@ -4,7 +4,7 @@
 
 import pendulum as dt
 import logging
-
+import time
 list_of_tickers=["TSLA","AMZN"]
 time_zone='America/New_York'
 strategy_name='stock_sma'
@@ -12,6 +12,40 @@ logging.basicConfig(level=logging.INFO, filename=f'{strategy_name}_{dt.now(tz=ti
 logging.info(f'starting {strategy_name} strategy file')
 print(f'starting {strategy_name} strategy file')
 
+
+def main_strategy_code():
+    print('main strategy started')
+
+
+
 current_time=dt.now(tz=time_zone)
 print(current_time)
 
+start_hour,start_min=7,35
+end_hour,end_min=7,38
+
+start_time=dt.datetime(current_time.year,current_time.month,current_time.day,start_hour,start_min,tz=time_zone)
+end_time=dt.datetime(current_time.year,current_time.month,current_time.day,end_hour,end_min,tz=time_zone)
+
+print(start_time)
+print(end_time)
+
+
+#this code will execute before the start time
+while dt.now(tz=time_zone)<start_time:
+    print(dt.now(tz=time_zone))
+    time.sleep(1)
+print('we have reached start time')
+
+
+
+while True:
+    if dt.now(tz=time_zone)>end_time:
+        break
+    ct=dt.now(tz=time_zone)
+    print(ct)
+    
+    if ct.second==1: #and ct.minute in range(0,60,5):#[0,5,10,15..55]
+        main_strategy_code()
+    time.sleep(1)
+print('strategy stopped')
