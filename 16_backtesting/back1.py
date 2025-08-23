695125ec35ee9a93091d562b9d456b34b23059dc
sunilsala88 <sunilsala88@gmail.com>
2025-08-23 17:27:46 +0530

    aug-23-4

--- 16_backtesting/back1.py
+++ 16_backtesting/back1.py
@@ -21,12 +21,21 @@ def get_sma(close_price,lengtht):
     return sma
 
 class sma_strategy(Strategy):
-    
+    s1=20
+    s2=50
     def init(self):
-        pass
+        self.sma1=self.I(get_sma,self.data.df.Close,self.s1)
+        self.sma2=self.I(get_sma,self.data.df.Close,self.s2)
 
     def next(self):
-        pass
+        if self.sma1[-1]>self.sma2[-1] and self.sma1[-2]<self.sma2[-2]:
+            if self.position.is_short:
+                self.position.close()
+            self.buy()
+        elif self.sma1[-1]<self.sma2[-1] and self.sma1[-2]>self.sma2[-2]:
+            if self.position.is_long:
+                self.position.close()
+            self.sell()
 
 bt=Backtest(data,sma_strategy,cash=1000)
 result=bt.run()
