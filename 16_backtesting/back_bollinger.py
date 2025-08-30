a1d0505b28cb525b239bd45c5946f30b87b9a41a
sunilsala88 <sunilsala88@gmail.com>
2025-08-30 16:44:52 +0530

    aug-30-1

--- 16_backtesting/back_bollinger.py
+++ 16_backtesting/back_bollinger.py
@@ -1,57 +1,65 @@
+from backtesting import Backtest, Strategy
+# import indicators as ta
+import pandas_ta as ta 
 
-from backtesting import Strategy,Backtest
-import pandas_ta as ta
-import yfinance as yf
-data=yf.download('GOOG',period='3y',interval='1d',multi_level_index=False)
-print(data)
+def upper1(close,length):
+    return ta.bbands(close,length)[f'BBU_{length}_2.0']
 
-def upper(close,l):
-    d=ta.bbands(close,l)
-    print(d)
-    return d[f'BBU_{l}_2.0']
-
-def lower(close,l):
-    d=ta.bbands(close,l)
-    print(d)
-    return d[f'BBL_{l}_2.0']
-
-def middle(close,l):
-    d=ta.bbands(close,l)
-    print(d)
-    return d[f'BBM_{l}_2.0']
+def lower1(close,length):
+    return ta.bbands(close,length)[f'BBL_{length}_2.0']
 
 
-class Bollinger(Strategy):
-    bl=30
+class Bollinger_s(Strategy):
+    n1 = 10
+
+
     def init(self):
-        self.upper=self.I(upper,self.data.df.Close,self.bl)
-        self.lower=self.I(lower,self.data.df.Close,self.bl)
-        self.middle=self.I(middle,self.data.df.Close,self.bl)
+        close = self.data.Close.s
+        self.upper = self.I(upper1, close, self.n1)
+        self.lower = self.I(lower1, close, self.n1)
+
 
     def next(self):
-        
-        # if not self.position:
-            if self.lower[-1]>self.data.Close[-1] and not  self.position:
-                self.buy()
-            elif self.upper[-1]<self.data.Close[-1] and not self.position:
-                self.sell()
-            
-            if self.position.is_long and self.upper[-1]<self.data.Close[-1]:
-                 self.position.close()
-            elif self.position.is_short and self.lower[-1]>self.data.Close[-1]:
-                 self.position.close()        
-        # if self.position.is_long and self.data.Close[-1]>self.middle[-1]:
-        #     self.position.close()
-        # elif self.position.is_short and self.data.Close[-1]<self.middle[-1]:
-        #     self.position.close()
+        close_price=self.data.Close[-1]
+        current_upper=self.upper[-1]
+        current_lower=self.lower[-1]
+        if close_price<current_lower:
+            self.position.close()
+            self.buy()
+        elif close_price>current_upper:
+            self.position.close()
+            self.sell()
 
 
-bt=Backtest(data,Bollinger,cash=1000000,commission=0.01)
-result=bt.run()
-print(result)
-# bt.plot()
 
-output=bt.optimize(bl=range(10,100,5),maximize='Return [%]')
-print(output)
-print(output['_strategy'])
+
+import yfinance as yf
+data=yf.download('GOOG',period='5y',multi_level_index=False)
+print(data)
+
+b1=ta.bbands(data['Close'],10)
+print(b1)
+
+
+bt = Backtest(data, Bollinger_s,
+              cash=10000, commission=.002,
+              exclusive_orders=True,trade_on_close=True,finalize_trades=True)
+
+output = bt.run()
 bt.plot()
+print(output)
+
+# # output['_trades'].to_csv('trades.csv')
+
+# #optimize
+
+
+def custom_optimization(stats):
+    return stats['Return [%]']
+
+
+stats=bt.optimize(n1=range(5,50,2),maximize=custom_optimization)
+print(stats)
+print(stats['_strategy'])
+bt.plot()
+stats['_trades'].to_csv('trades.csv')
